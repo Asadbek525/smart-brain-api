c57dbc2c01b39da8c6b000fa724cd404823bf4d7
Asadbek525 <68222862+Asadbek525@users.noreply.github.com>
2021-02-02 22:53:14 +0500

    first commit

--- app.js
+++ app.js
@@ -2,56 +2,55 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
-const { handleImage } = require('./controllers/image');
+const { handleImage , handleApiCall } = require('./controllers/image');
 const { handleProfile } = require('./controllers/profile');
 const { handleRegister } = require('./controllers/register');
 const { handleSignin } = require('./controllers/signin');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : 'localhost',
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
-    }
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'test',
+    database: 'smart-brain',
+  },
 });
-
 
 const app = express();
 
-
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 /**
  * Home Page
  */
-app.get('/',(req,res)=>{
-    res.json("Success")}
-);
+app.get('/', (req, res) => {
+  res.json('Success');
+});
 
 /**
  * Sign In Page
  */
-app.post('/signin',handleSignin(bcrypt,db));
+app.post('/signin', handleSignin(bcrypt, db));
 /**
  * Register page
  */
-app.post('/register',handleRegister(bcrypt,db));
+app.post('/register', handleRegister(bcrypt, db));
 
 /**
- * Profile route    
+ * Profile route
  */
 
-app.get('/profile/:id',handleProfile)
+app.get('/profile/:id', handleProfile());
 
 /**
  * Image entries
  */
 
-app.put('/image', handleImage(db))
+app.put('/image', handleImage(db));
+app.post('/imageurl', handleApiCall());
 
-app.listen(3000,()=>{
-    console.log("app is listening on port 3000");
-})
+app.listen(3000, () => {
+  console.log('app is listening on port 3000');
+});
--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,16 @@
+const Clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+    apiKey: '20c74c40ee924c9ea8c1658f7782a796',
+});
+
+const handleApiCall = () => (req,res) => {
+    app.models.predict(Clarifai.FACE_EMBED_MODEL,req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json("Error"))
+
+}
 const handleImage = (db) => (req,res)=>{
     const { id } = req.body;
     db('users').where({id})
@@ -7,4 +20,4 @@ const handleImage = (db) => (req,res)=>{
         .catch(err => res.status(400).json("Unable to get entries"));
 }
 
-module.exports = { handleImage }
+module.exports = { handleImage,  handleApiCall }
